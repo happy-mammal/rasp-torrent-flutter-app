959ae10cfd3bdbf704f735c4f47c4dab898d17cd
pjnichal <nichal.pravin2001@gmail.com>
2021-07-09 21:04:17 +0530

    remove hydrate

--- lib/business_logic/cubits/theme_cubit/theme_state.dart
+++ lib/business_logic/cubits/theme_cubit/theme_state.dart
@@ -4,21 +4,4 @@ class ThemeState {
   final ThemeMode themeMode;
 
   ThemeState({required this.themeMode});
-
-  Map<String, dynamic> toMap() {
-    return {
-      'themeMode': themeMode,
-    };
-  }
-
-  factory ThemeState.fromMap(Map<String, dynamic> map) {
-    return ThemeState(
-      themeMode: map['themeMode'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory ThemeState.fromJson(String source) =>
-      ThemeState.fromMap(json.decode(source));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
+
 import 'package:rasp_torrent_flutter_app/business_logic/cubits/theme_cubit/theme_cubit.dart';
 import 'package:rasp_torrent_flutter_app/router/approute.dart';
 import 'package:rasp_torrent_flutter_app/themes/app_theme.dart';
@@ -11,8 +10,6 @@ import 'package:sizer/sizer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HydratedBloc.storage = await HydratedStorage.build(
-      storageDirectory: await getApplicationDocumentsDirectory());
 
   runApp(RaspTorrent());
 }
