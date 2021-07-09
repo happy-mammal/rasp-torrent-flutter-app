991b7726444aa95c91044ce7b31c9b7850c87ca6
pjnichal <nichal.pravin2001@gmail.com>
2021-07-09 13:46:28 +0530

    'stateful to stateless'

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,7 @@ void main() {
   runApp(RaspTorrent());
 }
 
-class RaspTorrent extends StatefulWidget {
-  const RaspTorrent({Key? key}) : super(key: key);
-
-  @override
-  _RaspTorrentState createState() => _RaspTorrentState();
-}
-
-class _RaspTorrentState extends State<RaspTorrent> {
+class RaspTorrent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Sizer(
@@ -22,7 +15,8 @@ class _RaspTorrentState extends State<RaspTorrent> {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           home: HomeScreen(),
-          theme: ThemeData(primaryColor: Colors.green,accentColor: Colors.white),
+          theme:
+              ThemeData(primaryColor: Colors.green, accentColor: Colors.white),
         );
       },
     );
