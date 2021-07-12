17a26729572eeff330bffdccd37cfb82914bba56
Happy Mammal <yashlalit.23@gmail.com>
2021-07-11 20:13:58 -0700

    Added About dev

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -40,7 +40,9 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           actions: [
             GestureDetector(
-                onTap: () {},
+                onTap: () {
+                  Navigator.of(context).pushNamed('/addtorrent');
+                },
                 child: Icon(
                   Icons.add,
                   size: 30,
@@ -83,7 +85,205 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
-        drawer: Drawer(),
+        drawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 20.h,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 13.h,left: 3.h),
+                  child: Text(
+                    "About devs",
+                    style: GoogleFonts.poppins(
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/bg.jpg"),
+                    fit: BoxFit.cover
+                  ),
+                ),
+              ),
+              SizedBox(height: 1.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 2.h),
+                    child: CircleAvatar(
+                      radius: 4.h,
+                      backgroundImage: AssetImage("images/pravin.jpeg"),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.only(left: 2.h),
+                    child: Text(
+                      "Pravin Nichal",
+                      style: GoogleFonts.poppins(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+              SizedBox(height: 3.h),
+              GestureDetector(
+                onTap: (){},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 4.h),
+                      child: SizedBox(
+                        width: 3.h,
+                        height: 3.h,
+                        child: Image.asset("images/instagram.png"),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 2.h),
+                      child: Text(
+                        "Instagram",
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 3.h),
+              GestureDetector(
+                onTap: (){},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 4.h),
+                      child: SizedBox(
+                        width: 3.h,
+                        height: 3.h,
+                        child: Image.asset("images/linkedin.png"),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 2.h),
+                      child: Text(
+                        "LinkedIn",
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 1.h),
+              Divider(),
+              SizedBox(height: 1.h),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 2.h),
+                      child: CircleAvatar(
+                          radius: 4.h,
+                          backgroundImage: AssetImage("images/yash.jpg"),
+                        ),
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.only(left: 2.h),
+                      child: Text(
+                            "Yash Lalit",
+                            style: GoogleFonts.poppins(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                    ),
+
+                  ],
+                ),
+              SizedBox(height: 3.h),
+              GestureDetector(
+                onTap: (){},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 4.h),
+                      child: SizedBox(
+                        width: 3.h,
+                        height: 3.h,
+                        child: Image.asset("images/instagram.png"),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 2.h),
+                      child: Text(
+                        "Instagram",
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 3.h),
+              GestureDetector(
+                onTap: (){},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 4.h),
+                      child: SizedBox(
+                        width: 3.h,
+                        height: 3.h,
+                        child: Image.asset("images/linkedin.png"),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 2.h),
+                      child: Text(
+                        "LinkedIn",
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 1.h),
+              Divider(),
+
+            ],
+          ),
+        ),
         body: SafeArea(
           child: BlocConsumer<GetTorrentBloc, GetTorrentState>(
               listener: (context, state) async {
