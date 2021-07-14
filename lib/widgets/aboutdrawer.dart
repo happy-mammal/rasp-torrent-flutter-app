8dd3a9c75dd9c009284a6e0a5527e487e1731f84
pjnichal <nichal.pravin2001@gmail.com>
2021-07-14 15:11:52 +0530

    dsfsd

--- lib/widgets/aboutdrawer.dart
+++ lib/widgets/aboutdrawer.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutDrawer extends StatelessWidget {
   const AboutDrawer({Key? key}) : super(key: key);
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +68,9 @@ class AboutDrawer extends StatelessWidget {
           ),
           SizedBox(height: 3.h),
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _launchURL('https://www.instagram.com/pravin_nichal/');
+            },
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -86,89 +98,10 @@ class AboutDrawer extends StatelessWidget {
           ),
           SizedBox(height: 3.h),
           GestureDetector(
-            onTap: () {},
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 4.h),
-                  child: SizedBox(
-                    width: 3.h,
-                    height: 3.h,
-                    child: Image.asset("images/linkedin.png"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 2.h),
-                  child: Text(
-                    "LinkedIn",
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 1.h),
-          Divider(),
-          SizedBox(height: 1.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 2.h),
-                child: CircleAvatar(
-                  radius: 4.h,
-                  backgroundImage: AssetImage("images/yash.jpg"),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 2.h),
-                child: Text(
-                  "Yash Lalit",
-                  style: GoogleFonts.poppins(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w500,
-                    fontStyle: FontStyle.normal,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 3.h),
-          GestureDetector(
-            onTap: () {},
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 4.h),
-                  child: SizedBox(
-                    width: 3.h,
-                    height: 3.h,
-                    child: Image.asset("images/instagram.png"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 2.h),
-                  child: Text(
-                    "Instagram",
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 3.h),
-          GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _launchURL(
+                  'https://www.linkedin.com/in/pravin-nichal-93080b1b4/}');
+            },
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
